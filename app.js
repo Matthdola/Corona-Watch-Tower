c4c5dee82edfdc4cdf3761cf8d6d74d9e3ff3955
Mathdola <matthiasdola@gmail.com>
2020-03-27 15:58:27 +0000

    Change client folder

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}) );
 
 app.use( express.static(__dirname + '/client' ) );
-
+console.log(__dirname + '/client');
 var listener = server.listen(process.env.PORT || 5000, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 5000
 });
