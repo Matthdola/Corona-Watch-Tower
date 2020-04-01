82d7b0cc5811a3bdb1705b42e442a9b56b0c244f
Mathdola <matthiasdola@gmail.com>
2020-04-01 21:35:52 +0000

    Add firebase env config to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDWLl2nOoKzKZC3wXdYL99V_K8kiSGV4kE',
+    authDomain: 'coronawatchtower.firebaseapp.com',
+    databaseURL: 'https://coronawatchtower.firebaseio.com',
+    projectId: 'coronawatchtower',
+    storageBucket: 'coronawatchtower.appspot.com',
+    messagingSenderId: '742447493408',
+    appId: '1:742447493408:web:2980c00010b87bc04ce745',
+    measurementId: 'G-WQ2HM017R1'
+  }
 };
